065febc7384f3345281038844e5406f7d4bfc7f1
Rage-100 <forcocgamegamil007@gmail.com>
2026-04-11 18:18:51 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
 attendance = float(input("Enter attendance percentage: "))  
 if attendance >= 75:  
-print("Student is Eligible for Exam")  
+    print("Student is Eligible for Exam")  
 else:  
-print("Student is Not Eligible for Exam")
+    print("Student is Not Eligible for Exam") 
